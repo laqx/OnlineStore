652b3b2dd1ae94cd7886c5bb969a6eefc8789245
Natalija Hamandikova <test@test.lv>
2021-11-27 09:26:37 +0200

    "UserDao"

--- src/main/java/OnlineStoreApp/demo/dataBaseAccessObjects/AddToDatabase.java
+++ src/main/java/OnlineStoreApp/demo/dataBaseAccessObjects/AddToDatabase.java
@@ -5,13 +5,7 @@ import java.sql.*;
 public class AddToDatabase {
     public static void main(String[] args) {
         try {
-            Connection connection = DriverManager.getConnection(
-
-                   "jdbc:mysql://localhost:3306/OnlineStore", "root", "Oxford1984");
-//            "jdbc:mysql://localhost:3306/onlinestore ", "root", "root");
-//                    "jdbc:mysql://127.0.0.1:3306/OnlineStore", "root", "Oxford1984");
-//            "jdbc:mysql://localhost:3306/onlinestore ", "root", "root");
-//           "jdbc:mysql://localhost:1800/onlinestore", "root", "admin");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/OnlineStore", "root", "Oxford1984");
 
 //            addNewProduct(con, "roses", 3.00, 1, 55, 1);
 //            addNewSubCategory(con, "candles");
