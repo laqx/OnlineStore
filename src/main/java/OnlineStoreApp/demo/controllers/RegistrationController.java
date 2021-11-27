19d516d581024c0f171a99cd9ddddb4512b25ed9
laqx <84091012+laqx@users.noreply.github.com>
2021-11-27 11:15:39 +0100

    Merge remote-tracking branch 'origin/laine' into laine

--- src/main/java/OnlineStoreApp/demo/controllers/RegistrationController.java
+++ src/main/java/OnlineStoreApp/demo/controllers/RegistrationController.java
@@ -7,9 +7,7 @@ import OnlineStoreApp.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RegistrationController {
@@ -28,6 +26,7 @@ public class RegistrationController {
 
         return "register-success";
     }
+
 
 
 }
